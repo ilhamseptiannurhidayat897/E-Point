731e220a7a1eca0ff886533aac0b322ce8cfaec4
RafflyTrr <148837579+RafflyTrr@users.noreply.github.com>
2025-12-06 09:19:49 +0700

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login - E-Point SMKN 1 Kawali</title>
+    <title>Login - E-Point SMKN 1 Kawalii</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <style>
         @keyframes gradient {
