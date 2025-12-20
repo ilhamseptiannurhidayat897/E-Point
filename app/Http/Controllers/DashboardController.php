a2cd462d62046ba2f0205055c3b4acb68ad3287b
tiaraveraa <tiaraananta@gmail.com>
2025-12-20 10:14:50 +0700

    Resolve merge conflicts

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use App\Models\GoodPoint;
+use App\Models\Violation;
+use App\Models\Student;
 
 class DashboardController extends Controller
 {
@@ -20,5 +24,29 @@ class DashboardController extends Controller
     public function guru()
     {
         return view('dashboard.guru.dashboard');
+    }
+
+    public function riwayatPoin()
+    {
+        /** @var Student $siswa */
+        $siswa = Auth::user(); // IDE friendly
+
+        if (!$siswa) {
+            abort(403, "Siswa belum login.");
+        }
+
+        $good = GoodPoint::where('student_id', $siswa->id)
+            ->select('created_at', 'points as jumlah', 'description as keterangan')
+            ->addSelect(DB::raw("'kebaikan' as jenis"));
+
+        $bad = Violation::where('student_id', $siswa->id)
+            ->select('created_at', 'points as jumlah', 'description as keterangan')
+            ->addSelect(DB::raw("'pelanggaran' as jenis"));
+
+        $riwayat = $good->unionAll($bad)
+            ->orderBy('created_at', 'desc')
+            ->get();
+
+        return view('dashboard.siswa.laporan', compact('riwayat'));
     }
 }
